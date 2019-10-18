65ae0f316547e8ee205960243e8a682553d25ca2
Lokesh Devnani <contact@lokeshd.com>
2019-10-19 01:20:51 +0530

    Add Logging with LogRus

--- app/go.mod
+++ app/go.mod
@@ -2,4 +2,7 @@ module github.com/lokeshdevnani/hello-world-enterprise
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/sirupsen/logrus v1.4.2
+)
--- app/hello_server.go
+++ app/hello_server.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+  // Log as JSON instead of the default ASCII formatter.
+  log.SetFormatter(&log.JSONFormatter{})
+
+  // Output to stdout instead of the default stderr
+  log.SetOutput(os.Stdout)
+
+  // Only log the warning severity or above.
+  log.SetLevel(log.InfoLevel)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     name := r.URL.Query().Get("name")
     if name == "" {
         name = "World"
     }
+    log.Info("Received request for ", name)
     w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
 }
 
@@ -22,5 +36,8 @@ func main() {
         Handler:      r,
         Addr:         ":8080",
 	}
-	srv.ListenAndServe();
+	log.Info("Starting Server...")
+	if err := srv.ListenAndServe(); err != nil {
+        log.Fatal(err)
+	}
 }
