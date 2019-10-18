caf508c73fa9cabae6764dce7e18908bab96eae4
Lokesh Devnani <contact@lokeshd.com>
2019-10-19 01:00:54 +0530

    Basic Hello World HTTP Go

---
+++ app/hello_server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    name := "World"
+	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+
+	srv := &http.Server{
+        Handler:      r,
+        Addr:         ":8080",
+	}
+	srv.ListenAndServe();
+}
